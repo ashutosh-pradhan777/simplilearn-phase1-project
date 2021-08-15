5bf2b8e22b32d1cc7eca070e9032706e5ae7ac4c
ashupradhan <nightdreamer1111@gmail.com>
2021-08-16 01:44:03 +0530

    Final Design Done. Readme to be added

--- myscript2.js
+++ myscript2.js
@@ -4,9 +4,10 @@
 function fun(){
     var title = document.getElementById("title").value;
     var content = document.getElementById("content").value;
-    alert(title +" "+ content);
+    //alert(title +" "+ content);
     //document.getElementById("cont").innerHTML += "<h1>Hello</h1>";
     document.getElementById("cont").innerHTML += `<div class='card container-item' style='width: 18rem;'><img src='https://cdn.pixabay.com/photo/2015/04/20/13/17/work-731198__340.jpg' class='card-img-top' alt='...'>  <div class='card-body'><h5 class='card-title'>${title}</h5><p class='card-text'> ${content}</p><a href='#' class='btn btn-primary'>Click Here</a><i class='fas fa-comment-alt' style='margin-left: 40px;'></i></div></div>` ;
+    
 }
 
 function comment(event)
