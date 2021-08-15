aaafd771927ac9eed3baef3aa089f6b12a7402a0
ashupradhan <nightdreamer1111@gmail.com>
2021-08-16 00:34:05 +0530

    Functional Model. Few design changes and features to be added

---
+++ myscript.js
@@ -0,0 +1,10 @@
+var title; var content;
+function cont()
+{
+     title = document.getElementById("title").value;
+     content = document.getElementById("content").value;
+     alert(title);
+}
+
+
+
